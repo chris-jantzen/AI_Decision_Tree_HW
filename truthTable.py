ed73eb0bb3ae5499cdd8229382fda3d3873c0bc9
Chris Jantzen <jantzeca@mail.uc.edu>
2019-11-16 12:14:13 -0500

    structure

--- truthTable.py
+++ truthTable.py
@@ -1,10 +1,20 @@
 from pandas import read_excel
 from random import sample
 
-table = read_excel("P4Data.xlsx")
 
-data_sets = []
-for x, y, c in zip(table['X'], table['Y'], table['Class']):
-    data_sets.append([x, y, c])
+def read_data():
+    table = read_excel("P4Data.xlsx")
+    data_sets = []
+    for x, y, c in zip(table['X'], table['Y'], table['Class']):
+        data_sets.append([x, y, c])
+    return data_sets
 
-training_indices = sample(range(len(table)), int(len(table)*.70))
+
+def main():
+    data_sets = read_data()
+    # Randomly pick 70% of data for training
+    training_indices = sample(range(len(table)), int(len(table)*.70))
+
+
+if __name__ == "__main__":
+    main()
