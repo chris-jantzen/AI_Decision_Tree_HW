736a11b2279e686be6a80e7f989fdf796bfae1bc
Chris Jantzen <jantzeca@mail.uc.edu>
2019-11-16 12:23:02 -0500

    get testing indicies

--- truthTable.py
+++ truthTable.py
@@ -10,10 +10,20 @@ def read_data():
     return data_sets
 
 
+def get_testing_indicies(training_indices, data_size):
+    testing_indicies = []
+    for index in range(data_size):
+        if index not in training_indices:
+            testing_indicies.append(index)
+    return testing_indicies
+
+
 def main():
     data_sets = read_data()
+
     # Randomly pick 70% of data for training
     training_indices = sample(range(len(table)), int(len(table)*.70))
+    testing_indices = get_testing_indicies(training_indices, len(table))
 
 
 if __name__ == "__main__":
