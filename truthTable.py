7837a123987eef03bacab93dbb27f52d09a27a7b
Chris Jantzen <jantzeca@mail.uc.edu>
2019-11-20 18:01:06 -0500

    Finish

--- truthTable.py
+++ truthTable.py
@@ -1,13 +1,19 @@
 from pandas import read_excel
 from random import sample
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
 
 
 def read_data():
     table = read_excel("P4Data.xlsx")
     data_sets = []
+    classes = []
     for x, y, c in zip(table['X'], table['Y'], table['Class']):
-        data_sets.append([x, y, c])
-    return data_sets
+        data_sets.append([x, y])
+        classes.append([c])
+    return (data_sets, classes)
 
 
 def get_testing_indicies(training_indices, data_size):
@@ -18,12 +24,67 @@ def get_testing_indicies(training_indices, data_size):
     return testing_indicies
 
 
-def main():
-    data_sets = read_data()
+def build_50_by_50():
+    lst = []
+    for i in range(1, 51):
+        for j in range(1, 51):
+            lst.append([i, j])
+    return lst
 
-    # Randomly pick 70% of data for training
-    training_indices = sample(range(len(table)), int(len(table)*.70))
-    testing_indices = get_testing_indicies(training_indices, len(table))
+
+def get_x_coords(coords):
+    x_coords = []
+    for coord in coords:
+        x_coords.append(coord[0])
+    return x_coords
+
+
+def get_y_coords(coords):
+    y_coords = []
+    for coord in coords:
+        y_coords.append(coord[1])
+    return y_coords
+
+
+def create_graph(coords, pred):
+    # x_coords = get_x_coords(coords)
+    # y_coords = get_y_coords(coords)
+    pred0x = []
+    pred0y = []
+    pred1x = []
+    pred1y = []
+    for i in range(len(pred)):
+        if pred[i] == 0:
+            pred0x.append(coords[i][0])
+            pred0y.append(coords[i][1])
+        else:
+            pred1x.append(coords[i][0])
+            pred1y.append(coords[i][1])
+
+    plt.plot(pred0x, pred0y, 'rs', pred1x, pred1y, 'bs')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.axis([0, 50, 0, 50])
+    plt.show()
+
+
+def main():
+    (data_sets, classes) = read_data()
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        data_sets, classes, test_size=.30)
+
+    classifier = DecisionTreeClassifier()
+    classifier.fit(x_train, y_train)
+
+    y_pred = classifier.predict(x_test)
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
+
+    n_test = build_50_by_50()
+    n_pred = classifier.predict(n_test)
+
+    create_graph(n_test, n_pred)
 
 
 if __name__ == "__main__":
